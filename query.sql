f4ae434b7bb182371a2dc85175122266fa885dfa
catdog50rus <49200747+catdog50rus@users.noreply.github.com>
2021-01-29 18:44:19 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,3 +1,5 @@
 SELECT p.product_name, c.category_name
 FROM products p
-LEFT JOIN (products_by_category pc INNER JOIN category c ON pc.category_id = c.category_id) ON p.product_id = pc.product_id;
+LEFT JOIN 
+    (products_by_category pc INNER JOIN category c ON pc.category_id = c.category_id) 
+ON p.product_id = pc.product_id;
